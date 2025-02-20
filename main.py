739913f758e3435229c5409afddd31e64cd1e7d9
Mogaladi Brilliant <masemola.m98@gmail.com>
2025-02-20 20:24:12 +0200

    calculate area

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import mathr = float(input("Enter radius or diameter: "))
+import math
 
+unit=input("what do you have, radius or diameter? ")
 
-Area = round((math.pi*pow(r,2)),2)
-print(f"Area = {Area}")
+if unit == "radius":
+      r = float(input("what is radius: "))
+      Area = round((math.pi * pow(r, 2)), 2)
+elif unit == "diameter":
+      d = float(input("Enter is diameter: "))
+      r=d/2
+      Area = round((math.pi * pow(r, 2)), 2)
+else:
+      print("Wrong input")
+
