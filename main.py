00a7abd5b3faba2b806d47cc26a4eda6398eb6ee
Mogaladi Brilliant <masemola.m98@gmail.com>
2025-04-10 21:14:21 +0200

    Calculator

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-#calculator
-
-num1,sign, num2= input("Enter fisrt number, sign and second number: ").split()
-num1= float(num1)
-num2 = float(num2)
-
-if sign=="+":
-    print(f"The sum is : {num1 + num2}")
-elif sign=="-":
-    print(f"Answer is : {num1 - num2}")
-elif sign=="*":
-    print(f"Answer is  : {num1 * num2}")
-elif sign=="/":
-    print(f"Answer is  : {num1 / num2}")
-elif sign=="%":
-    print(f"Answer is  : {num1 % num2}")
-else:
-    print(f"invalid operator {sign}")
-
+print(help(str
+           ))
