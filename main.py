2efbc67112ecc201b541e49e8199a41ab942dec9
Mogaladi Brilliant <masemola.m98@gmail.com>
2025-04-15 16:59:32 +0200

    Calculator

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print(help(str
-           ))
+##ask a user for a name
+import time
+
+timecounter = int(input("what is you name: "))
+
+for x in reversed(range(0, timecounter)):
+    print(f"00:00:{x:02}")
+    time.sleep(1)
